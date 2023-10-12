b3fabd5948c9b73eedf79f91c039590482f57c75
Lian <lcb_faust@naver.com>
2023-10-12 17:01:19 +0900

    231012 member

--- src/main/java/com/example/project4/controller/MemberController.java
+++ src/main/java/com/example/project4/controller/MemberController.java
@@ -25,32 +25,32 @@ public class MemberController {
     @GetMapping(value ="/new")
     public String memberForm(Model model){
         model.addAttribute("memberFormDto", new MemberFormDto());
-        return "member/memberForm";
+        return "member/createAccount";
 
     }
     @PostMapping(value="/new")
     public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model){
         if (bindingResult.hasErrors()){
-            return "member/memberForm";
+            return "member/createAccount";
         }
         try{
             Member member = Member.createMember(memberFormDto, passwordEncoder);
         }
         catch (IllegalStateException e){
             model.addAttribute("erroMessage", e.getMessage());
-            return "member/memberForm";
+            return "member/createAccount";
         }
         return "redirect:/";
     }
 
     @GetMapping(value = "/login")
-    public String loginMember() {return  "member/memberLoginForm";}
+    public String loginMember() {return  "member/login";}
 
 
     @GetMapping(value = "/login/error")
     public String loginError(Model model){
      model.addAttribute("loginErrorMsg", "아이디 혹은 비밀번호를 맞게 입력해주세요");
-     return "/member/memberLoginForm";
+     return "/member/login";
     }
 
 
