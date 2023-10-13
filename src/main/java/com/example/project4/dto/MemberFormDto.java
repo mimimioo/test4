fe1689c7522636b3b76174dee882b2f408fdade3
Lian <lcb_faust@naver.com>
2023-10-13 17:42:18 +0900

    login create 구현

--- src/main/java/com/example/project4/dto/MemberFormDto.java
+++ src/main/java/com/example/project4/dto/MemberFormDto.java
@@ -21,9 +21,9 @@ public class MemberFormDto {
     @Length(min=8, max=16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요")
     private String password;
 
-    @NotEmpty(message = "비밀번호 확인은 필수 입력 값입니다")
-    @Length(min=8, max=16,message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요")
-    private String confirmPassword;
+//    @NotEmpty(message = "비밀번호 확인은 필수 입력 값입니다")
+//    @Length(min=8, max=16,message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요")
+//    private String confirmPassword;
 
 
     @NotEmpty(message = "주소는 필수 입력 값입니다.")
--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
     //입력폼 관련 이벤트 처리(회원가입)
-    const inputID = document.getElementById('id');
+    // const inputID = document.getElementById('id');
     const inputPW = document.getElementById('pw');
-    const inputPW_C = document.getElementById('pw_confirm');
+    // const inputPW_C = document.getElementById('pw_confirm');
     const inputEmail = document.getElementById('email');
-    const inputName = document.getElementById('name');
-    const idPlaceholder = document.getElementById('id_placeholder');
+    // const inputName = document.getElementById('name');
+    // const idPlaceholder = document.getElementById('id_placeholder');
     const pwPlaceholder = document.getElementById('pw_placeholder');
-    const namePlaceholder = document.getElementById('name_placeholder');
-    const pwCPlaceholder = document.getElementById('pw_C_placeholder');
+    // const namePlaceholder = document.getElementById('name_placeholder');
+    // const pwCPlaceholder = document.getElementById('pw_C_placeholder');
     const emailPlaceholder = document.getElementById('email_placeholder');
-    const tooltip_name = document.getElementById('tooltip_name');
-    const tooltip_id = document.getElementById('tooltip_id');
+    // const tooltip_name = document.getElementById('tooltip_name');
+    // const tooltip_id = document.getElementById('tooltip_id');
     const tooltip_pw = document.getElementById('tooltip_pw');
-    const tooltip_pwC = document.getElementById('tooltip_pwC');
+    // const tooltip_pwC = document.getElementById('tooltip_pwC');
     const tooltip_email = document.getElementById('tooltip_email');
     console.log("동작준비 완료");
 
-    console.log(inputID);
+    // console.log(inputID);
     // inputName.addEventListener('focus', () => {
     //
     //     $(namePlaceholder).addClass('focused');
@@ -32,20 +32,20 @@ $(document).ready(function() {
     //     $(tooltip_name).addClass('hidden');
     // });
 
-    inputID.addEventListener('focus', () => {
-        console.log("포커스 됨");
-        $(idPlaceholder).addClass('focused');
-        $(tooltip_id).removeClass('hidden');
-    });
-    inputID.addEventListener('blur', () => {
-        if(inputID.value==null || inputID.value=='') {
-            $(idPlaceholder).removeClass('focused');
-        } else {
-            $(idPlaceholder).addClass('focused');
-        }
-        $(tooltip_id).addClass('hidden');
-    });
-    
+    // inputID.addEventListener('focus', () => {
+    //     console.log("포커스 됨");
+    //     $(idPlaceholder).addClass('focused');
+    //     $(tooltip_id).removeClass('hidden');
+    // });
+    // inputID.addEventListener('blur', () => {
+    //     if(inputID.value==null || inputID.value=='') {
+    //         $(idPlaceholder).removeClass('focused');
+    //     } else {
+    //         $(idPlaceholder).addClass('focused');
+    //     }
+    //     $(tooltip_id).addClass('hidden');
+    // });
+
     inputPW.addEventListener('focus', () => {
         $(pwPlaceholder).addClass('focused');
         $(tooltip_pw).removeClass('hidden');
@@ -72,17 +72,17 @@ $(document).ready(function() {
     //     $(tooltip_pwC).addClass('hidden');
     // });
 
-    // inputEmail.addEventListener('focus', () => {
-    //     $(emailPlaceholder).addClass('focused');
-    //     $(tooltip_email).removeClass('hidden');
-    // });
-    // inputEmail.addEventListener('blur', () => {
-    //     if(inputEmail.value==null || inputEmail.value=='') {
-    //         $(emailPlaceholder).removeClass('focused');
-    //     } else {
-    //         $(emailPlaceholder).addClass('focused');
-    //     }
-    //     $(tooltip_email).addClass('hidden');
-    // });
+    inputEmail.addEventListener('focus', () => {
+        $(emailPlaceholder).addClass('focused');
+        $(tooltip_email).removeClass('hidden');
+    });
+    inputEmail.addEventListener('blur', () => {
+        if(inputEmail.value==null || inputEmail.value=='') {
+            $(emailPlaceholder).removeClass('focused');
+        } else {
+            $(emailPlaceholder).addClass('focused');
+        }
+        $(tooltip_email).addClass('hidden');
+    });
 });
 
