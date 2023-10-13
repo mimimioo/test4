0714751564bf1d07a1e78434388b48661edaf495
Lian <lcb_faust@naver.com>
2023-10-13 16:25:01 +0900

    member 뷰 이식

--- src/main/java/com/example/project4/controller/constant/Role.java
+++
@@ -1,5 +0,0 @@
-package com.example.project4.controller.constant;
-
-public enum Role {
-    USER, ADMIN
-}
--- src/main/java/com/example/project4/entity/Member.java
+++ src/main/java/com/example/project4/entity/Member.java
@@ -32,6 +32,12 @@ public class Member extends BaseEntity{
 
     public static Member createMember(MemberFormDto memberFormDto, PasswordEncoder passwordEncoder){
         Member member = new Member();
+        member.setName(memberFormDto.getName());
+        member.setEmail(memberFormDto.getEmail());
+        member.setAddress(memberFormDto.getAddress());
+        String password = passwordEncoder.encode(memberFormDto.getPassword());
+        member.setPassword(password);
+        member.setRole(Role.ADMIN);
         return member;
     }
 
