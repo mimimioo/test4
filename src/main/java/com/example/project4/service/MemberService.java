eeee9b698427269b1d7b60d07b83084349cb5c62
Lian-OwO <139307398+Lian-OwO@users.noreply.github.com>
2023-10-17 09:25:50 +0900

    MemberService.java 변경

--- src/main/java/com/example/project4/service/MemberService.java
+++ src/main/java/com/example/project4/service/MemberService.java
@@ -1,15 +1,19 @@
 package com.example.project4.service;
 
+import com.example.project4.dto.MemberFormDto;
 import com.example.project4.entity.Member;
 import com.example.project4.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -22,16 +26,16 @@ public class MemberService implements UserDetailsService {
         return memberRepository.save(member);
     }
 
-    public void validateDuplicateMember(Member member){
-        Member findMember = memberRepository.findByEmail(member.getEmail());
-        if(findMember != null){
+    public void validateDuplicateMember(Member member) {
+        Optional<Member> findMember = memberRepository.findByEmail(member.getEmail());
+        if (findMember.isPresent()) {
             throw new IllegalStateException("이미 가입되어 있습니다.");
         }
     }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
-        Member member = memberRepository.findByEmail(email);
+        Member member = memberRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(email));
         if(member == null){
             throw new UsernameNotFoundException(email);
         }
@@ -40,9 +44,20 @@ public class MemberService implements UserDetailsService {
                 .password(member.getPassword())
                 .roles(member.getRole().toString())
                 .build();
+
     }
 
-    public Long loadMemberId (String email){
-        return memberRepository.findByEmail(email).getId();
+
+    public MemberFormDto readMember(String email) throws UsernameNotFoundException{
+        Member member = memberRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(email));
+        MemberFormDto memberFormDto = MemberFormDto.of(member);
+        return memberFormDto;
+    }
+
+
+    public Member getMemberData(String email) {
+        // 데이터베이스에서 데이터 가져오는 로직
+        return memberRepository.findByEmail(email).orElseThrow(() -> new EntityNotFoundException("Member not found"));
     }
 }
+
